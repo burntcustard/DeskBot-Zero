9354dfff96a857ce0a733d688c04aa0b8477e147
burntcustard <burntcustard@gmail.com>
2017-11-12 21:47:48 +0000

    Test image saving in simpleTraining

--- src/Python/simpleTraining.py
+++ src/Python/simpleTraining.py
@@ -4,12 +4,19 @@
 # Training data collection program to take training images without
 # the robot falling off the edge of the desk or running into things
 
+# Python imports:
 import sys
 import time
 import math
 import random
 sys.path.insert(0, "../../lib/PiconZero/Python")
 
+# Camera imports:
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import cv2
+
+# DeskBot imports:
 import piconzero as pz
 import panTilt as panTilt
 import infrared as ir
@@ -21,16 +28,32 @@ panTilt.init()
 ir.init()
 robot.init()
 
-moveSpeed   = 35 # Movement speed 0-100
-delay       = 0.1 # The delay between movements and stuff in seconds
+moveSpeed = 35  # Movement speed 0-100
+delay     = 0.1 # The delay between movements and stuff in seconds
+res       = 128 # Camera resolution (# x # pixels)
+
+camera = PiCamera()
+camera.resolution = (res,res)
 
 try:
     while True:
 
+        # Take picture
+        rawCapture = PiRGBArray(camera)
+
+        # Wait while picture is taken (and stop loop going unreliably fast)
         time.sleep(delay)
+
+        # Convert image to Blue Green Red format (that's what OpenCV likes)
+        camera.capture(rawCapture, format="bgr")
 
         # Look around
         distanceToEdge, rotationToEdge = edge.getDistanceAndRotation()
+
+        # Save image with suitable filename
+        img = rawCapture.array
+        imgName = 'd' + str(distanceToEdge) + 'r' + str(rotationToEdge) + ".png"
+        cv2.imwrite(imgName, img)
 
         # If the robot is close-ish to the desk edge:
         if 4 <= distanceToEdge <= 8:
